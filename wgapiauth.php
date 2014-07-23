531cfad254d53ee1599b2e1c714f9cac7966f70f
strejla <strejla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 02:55:03 +0000

    'img'
    git-svn-id: https://plugins.svn.wordpress.org/wgapiauth/trunk@953188 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wgapiauth.php
+++ wgapiauth.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WGAPIAuth
 Plugin URI: http://wordpress.org/plugins/wgapiauth/
-Description: Плагин который позволяет пользователям авторизироваться при помощи Wargaming Public API. Cсылка для авторизации будет добавлена в форму добавления комментариев а так же на страницы авторизации и регистрации.
+Description: Плагин который позволяет пользователям авторизироваться при помощи Wargaming Public API.  Cсылка для авторизации будет добавлена в форму добавления комментариев а так же на страницы авторизации и регистрации.
 Version: 0.1
 Author: STREJlA
 Author URI: http://forum.worldoftanks.ru/index.php?/user/strejla-423825/
